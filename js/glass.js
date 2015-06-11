05aae023c39daccb2afc1f5a3e7fa601ea402a84
赵文宇 Wenyu ZHAO <654364111@qq.com>
2015-06-11 10:48:46 +1000

    1

---
+++ js/glass.js
@@ -0,0 +1,14 @@
+
+var glass_resize = function() {
+	var elements = document.getElementsByClassName("glass");
+	for(var ele in elements) {
+		var w = ele.clientWidth;
+		var h = ele.clientHeight;
+		ele.style.
+	}
+}
+
+
+window.addEventListener("onload", function(){
+	window.
+});
