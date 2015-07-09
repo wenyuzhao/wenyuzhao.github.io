9d947e6e6cbe263d511f831d46facc07451441c4
赵文宇 Wenyu ZHAO <654364111@qq.com>
2015-07-09 12:51:34 +1000

    0

---
+++ js/index.js
@@ -0,0 +1,18 @@
+$ = function() {
+    return document.querySelectorAll.apply(document, arguments);
+};
+
+
+function resizeTable {
+    autoCells = $("table.auto-grid > td");
+    for(var cell in autoCells) {
+        cell.style.height = cell.style.width;
+    }
+    cellImages = $("table.auto-grid > img");
+    for(var img in cellImages) {
+        img.style.maxWidth = img.parentNode.style.width;
+    }
+}
+
+window.addEventListener("load", resizeTable);
+window.addEventListener("resize", resizeTable);
