832c7656ffd10d300dca2fd5820e49699709022a
william <654364111@qq.com>
2016-09-22 20:42:39 +1000

    test

---
+++ src/home/index.coffee
@@ -0,0 +1,4 @@
+
+Three = require('three')
+
+console.log 'helloxyz'
---
+++ src/webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const webpack = require('webpack');
+const fs = require('fs');
+
+function getSubDirectories(folder) {
+    return fs.readdirSync(folder).filter(file =>
+        fs.statSync(path.join(folder, file)).isDirectory());
+}
+
+const exclude = /node_modules/;
+
+const BABEL_LOADER = {
+    exclude,
+    loader: 'babel',
+    test: /\.jsx?$/,
+    query: {
+        presets: ['es2015', 'stage-0', 'react'],
+    },
+};
+
+const COFFEE_LOADER = {
+    exclude,
+    loader: 'coffee-loader',
+    test: /\.coffee$/,
+};
+
+const entry = {};
+
+getSubDirectories(__dirname).forEach(name => {
+    entry[name] = [
+        path.join(__dirname, `common`),
+        path.join(__dirname, `${name}/index`),
+    ];
+});
+
+module.exports = {
+    entry,
+    output: {
+        path: path.join(__dirname, '../lib'),
+        filename: '[name].bundle.js',
+    },
+    resolve: {
+        extensions: [ '', '.js', '.jsx', '.json', '.coffee' ],
+    },
+    module: {
+        loaders: [
+            BABEL_LOADER,
+            COFFEE_LOADER,
+        ],
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({ compress: { warnings: false } }),
+    ],
+    devtool: '#cheap-module-eval-source-map',
+};
