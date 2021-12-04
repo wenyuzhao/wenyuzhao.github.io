8600bb6449ba82422deb4ef4e8de185644272632
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2021-12-04 22:10:22 +1100

    Support nested MDX page

--- utils/mdx-renderer.mjs
+++ utils/mdx-renderer.mjs
@@ -2,10 +2,13 @@ import matter from 'gray-matter';
 import { fromMarkdown } from 'mdast-util-from-markdown'
 import { mdxjs } from 'micromark-extension-mdxjs'
 import { mdxFromMarkdown } from 'mdast-util-mdx'
+import path from 'path';
+
+const LAYOUTS_DIR = path.resolve(path.resolve(), 'layouts');
 
 const WRAPPER = meta => `
 export const meta = ${JSON.stringify(meta)};
-import { ${meta.layout || 'default'} as Layout } from '../layouts';
+import { ${meta.layout || 'default'} as Layout } from '${LAYOUTS_DIR}';
 
 <Layout meta={meta}></Layout>
 `;
